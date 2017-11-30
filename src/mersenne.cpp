1fd6ea44f93bafeac1cdb5c1b7775bddab087937
easai <aksarih@gmail.com>
2017-11-30 10:04:51 -0500

    Added lucus_lehmer_test

--- src/mersenne.cpp
+++ src/mersenne.cpp
@@ -20,9 +20,38 @@ mpz_class mersenne(int p) {
 	return mp;
 }
 
+void _f(mpz_t r, int p) {
+	if (p == 0) {
+		mpz_set_ui(r, 4);
+	} else {
+		mpz_t t;
+		mpz_init(t);
+		_f(t, p - 1);
+		mpz_mul(r, t, t);
+		mpz_clear(t);
+		mpz_sub_ui(r, r, 2);
+	}
+}
+
+void lucus_lehmer_test(int p) {
+	mpz_t f, mod;
+	mpz_init(f);
+	mpz_init(mod);
+	mpz_class mpc = mersenne(p);
+
+	_f(f, p - 2);
+	mpz_mod(mod, f, mpc.get_mpz_t());
+
+	if (mpz_cmp_ui(mod, 0) == 0)
+		cout << mpc << " is a prime" << endl;
+	else
+		cout << mpc << " is not a prime" << endl;
+
+	mpz_clear(f);
+	mpz_clear(mod);
+}
+
 int main(int argc, char* argv[]) {
-	int nopt = 0;
-	char *nparam = NULL;
 	int opt;
 	while ((opt = getopt(argc, argv, "h")) != -1) {
 		switch (opt) {
